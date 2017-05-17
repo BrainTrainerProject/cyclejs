1b0fe7b5ed6807dc305b7e63c1ea1d20e149f28f
Andrej <sperling@protonmail.com>
2017-05-17 21:47:09 +0200

    Build take 4

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const buildExt = require('./tasks/build-extern');
 
 function buildWebApp(path) {
 
-    console.log("Try to create folder: " + path);
+    /*console.log("Try to create folder: " + path);
 
     if (path === undefined) {
         console.error("No path was submitted")
     } else {
         buildExt.build(path);
-    }
+    }*/
 
 }
 
