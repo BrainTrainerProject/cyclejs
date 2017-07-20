1ea37e9f83372a914b312412311ce83411c01a56
Andrej <sperling@protonmail.com>
2017-07-20 21:20:45 +0200

    Fixes TSLint issues and added syncDelay

--- src/common/Utils.ts
+++ src/common/Utils.ts
@@ -1,48 +1,54 @@
-import {Reducer, Sinks} from './interfaces';
+import {Sinks} from './interfaces';
 import {extractSinks, filterProp} from 'cyclejs-utils';
-import xs, {Stream} from 'xstream';
+import xs from 'xstream';
+
 const config = require('../config.json');
 
 export class Utils {
 
-    static slashCheck(url: string): string {
+    public static slashCheck(url: string): string {
         return url.charAt(0) === '/' ? url.substring(1) : url;
     }
 
-    static apiUrl(urlCall) {
+    public static apiUrl(urlCall: string): string {
         return config.apiUrl + Utils.slashCheck(urlCall);
-    };
+    }
 
-    static imageUrl(url) {
+    public static imageUrl(url: string): string {
         return '/src/img/' + Utils.slashCheck(url);
-    };
+    }
 
-    static imageOrPlaceHolder(url) {
-        if (url === '' || url === null || url === undefined) {
-            return '/src/img/' + Utils.slashCheck('/card-placeholder.png');
-        } else {
-            return url;
-        }
-    };
+    public static imageOrPlaceHolder(url: string): string {
+        return (url) ? url : '/src/img/' + Utils.slashCheck('/card-placeholder.png');
+    }
 
-    static jsonHasChilds(json) {
-        for (let itm in json)
+    public static jsonHasChilds(json: any): boolean {
+        for (const itm in json) {
             return true;
+        }
         return false;
     }
 
-    static filterPropsByArray(sinks: any, props: string[]) {
+    public static filterPropsByArray(sinks: any, props: string[]): any {
         let currSinks: Sinks = sinks;
-        for (let prop of props) {
+        for (const prop of props) {
             currSinks = filterProp(extractSinks(xs.of(currSinks), Object.keys(currSinks)), prop);
         }
         return currSinks;
 
     }
 
-    static isNumber(number){
+    public static isNumber(number: any): boolean {
         const res = parseInt(number);
         return !isNaN(res);
+    }
+
+    public static syncDelay(ms: number): void {
+        const curr = Date.now();
+        let passed = 0;
+        while (passed < ms) {
+            passed = Date.now() - curr;
+        }
     }
 
 }
